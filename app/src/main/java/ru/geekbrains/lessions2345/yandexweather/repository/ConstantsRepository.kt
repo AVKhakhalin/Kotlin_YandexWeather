e8d3e87fd4a8e34811664f88402887d820816753
Khakhalin Andrey Vladimirovich <avkhakhalin@mail.ru>
2021-09-07 05:52:30 +0300

    Выполнение 2 пункта ТЗ.
    Добавлено в манифест разрешение на выход в Интернет.
    Добавлены константы в класс "repository\ConstantsRepository" для работы с сервером Yandex.
    Подключены в файле "gradle" библиотеки "retrofit" и "gson".
    Проверено наличие уведомления в файле "gradle" об использовании "java" 8 версии.
    
    Ссылка на репозиторий проекта:
    https://github.com/AVKhakhalin/Kotlin_YandexWeather

--- app/src/main/java/ru/geekbrains/lessions2345/yandexweather/repository/ConstantsRepository.kt
+++ app/src/main/java/ru/geekbrains/lessions2345/yandexweather/repository/ConstantsRepository.kt
@@ -1,4 +1,23 @@
 package ru.geekbrains.lessions2345.yandexweather.repository
 
 class ConstantsRepository {
+    companion object {
+        // Константы для получения данных о погоде
+        @JvmField
+        val baseUrl : String = "https://api.weather.yandex.ru/"
+        @JvmField
+        val endPoint : String = "v2/informers"
+        @JvmField
+        val yandexKeyName : String = "X-Yandex-API-Key"
+        @JvmField
+        val yandexKeyValue : String = "ebbee072-d212-420e-9f62-4d716b0499e9"
+        @JvmField
+        val latName : String = "lat"
+        @JvmField
+        val lonName : String = "lon"
+        @JvmField
+        val langName : String = "lang"
+
+        // Константы для локальных настроек программы
+    }
 }
